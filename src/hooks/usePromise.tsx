ae1bf4216ca0fc46a15388c1ef6eedf3a12097e6
Daphne Smit <daphnesmit1986@gmail.com>
2021-01-27 09:22:36 +0100

    fix: add fetcherFn and export run

--- src/hooks/usePromise.tsx
+++ src/hooks/usePromise.tsx
@@ -1,24 +1,32 @@
-import { useRef } from 'react';
+import { useCallback, useRef } from 'react';
+import { fetcherFn } from '../types';
 
 import { State } from './reducer';
 import { useBasePromise, UsePromiseOptions } from './useBasePromise';
 import { usePromiseResultCache } from './usePromiseResultCache';
+interface IUsePromiseAsync<T> extends State<T> {
+  run: () => void
+}
 
-function usePromise<T>(id: string, fetcher: Promise<T>, options?: UsePromiseOptions): State<T> {
+function usePromise<T>(id: string, fetcher: fetcherFn<T>, options?: UsePromiseOptions): IUsePromiseAsync<T> {
   const hasFetched = useRef(false);
   const { state, setState } = usePromiseResultCache<T>(id);
   const fetchData = useBasePromise<T>(id, fetcher, options, setState);
 
+  const run = useCallback(() => {
+    fetchData({ ssr: false });
+  }, [fetchData]);
+
   // If data is cached return data
   if (state.data) {
-    return state;
+    return { ...state, run };
   }
 
   // No cache found, fetch data once
-  if (!hasFetched.current) fetchData();
+  if (!hasFetched.current && !options?.skip) fetchData();
   hasFetched.current = true;
 
-  return state;
+  return { ...state, run };
 }
 
 export { usePromise };
